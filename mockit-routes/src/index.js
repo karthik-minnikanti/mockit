e3de5a9d828405d8ab2d2cc1b3bd09b0158ab521
Karthik Minnikanti <karthikminniknati@gmail.com>
2024-08-12 15:01:41 +0530

    removed dotenv requirement

--- mockit-routes/src/index.js
+++ mockit-routes/src/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const axios = require("axios")
-require('dotenv').config()
 
 const port = process.env.PORT || 3000;
 
